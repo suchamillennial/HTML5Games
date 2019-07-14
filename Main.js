57608a73171c7f5671733d0e079006af5fe0c6a0
Tej <tejgidvani@hotmail.com>
2019-07-14 04:50:36 -0500

    Split the Main JS file further by removing the common graphics methods and putting them into their own file and then including that in a script tag in the HTML file

--- Main.js
+++ Main.js
@@ -225,39 +225,7 @@
             
         }
         
-        function drawBitmapCenteredWithRotation(useBitmap, atX, atY, withAng){
-            // Save the canvas properties
-            canvasContext.save();
-            // Moves car to the top (0,0)
-            canvasContext.translate(atX, atY);
-            // rotates car arount its center
-            canvasContext.rotate(withAng);
-            // move and draw the bitmap back at its correct location
-            canvasContext.drawImage(useBitmap, -useBitmap.width/2, -useBitmap.height/2);
-            // restore the canvas settings for working on the next images
-            canvasContext.restore();
-        }
-
-        // Refactored method for drawing rectangle
-        function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, boxColor){
-            canvasContext.fillStyle = boxColor;
-            canvasContext.fillRect(topLeftX,topLeftY,boxWidth,boxHeight);
-        }
         
-        // Refactored Method for drawing the car
-        function colorCircle(centerX, centerY, radius, fillColor) {
-            // Use Arc to draw a circle 
-            canvasContext.fillStyle = fillColor;
-            canvasContext.beginPath();
-            canvasContext.arc(centerX,centerY,radius,0,2*Math.PI, true);
-            canvasContext.fill();
-        }
-        
-        // Create function for writing text
-        function colorText(showWords,textX,textY,fillColor){
-            canvasContext.fillStyle = fillColor;
-            canvasContext.fillText(showWords,textX,textY);
-        }
         
         // This function calculates the index of a specific track
         function rowColToArrayIndex(col, row) {
