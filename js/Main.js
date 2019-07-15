66c683e861ec5c82c362ccb8177bdf0714003f52
Tej <tejgidvani@hotmail.com>
2019-07-15 01:57:42 -0500

    format main js file

--- js/Main.js
+++ js/Main.js
@@ -1,65 +1,67 @@
 
-        // Set up canvas variables
-        var canvas, canvasContext;
-        
-        carReset();
-
-        
-
-        window.onload = function() {
-            // Attach the HTML canvas to a variable
-            canvas = document.getElementById("gameCanvas");
-            // Get the graphics buffer for doing graphics
-            canvasContext = canvas.getContext('2d');
-            
-            // Add loading screen
-            colorRect(0, 0, canvas.width, canvas.height, 'red');
-            colorText("LOADING...", canvas.width/2, canvas.height/2, 'black');
-            // Load Images before everything else
-            loadImages();
-
-        }
-
-        // This is a dumbass name
-        function imageLoadingDoneSoStartGame() {
-            // Set FPS and have the updateAll function refresh according to that FPS
-            var fps = 30;
-            setInterval(updateAll,1000/fps);
-            
-            setupInput();
+// Set up canvas variables
+var canvas, canvasContext;
 
 
-            carReset();
-        }
 
 
-        // This is the function for drawing to and updating the canvas
-        function updateAll() {
-            moveAll();
-            drawAll();    
-        }
+carReset();
 
-        function moveAll() {
-            carMove();
-            carTrackHandling();
-        }
 
-        function clearScreen() {
-            colorRect(0,0,canvas.width,canvas.height,"black");
-        }
 
-        function drawAll(){
-            drawTracks();
-            drawCar();
-            
-        }
-        
-        
-        
-        
+window.onload = function() {
+    // Attach the HTML canvas to a variable
+    canvas = document.getElementById("gameCanvas");
+    // Get the graphics buffer for doing graphics
+    canvasContext = canvas.getContext('2d');
+    
+    // Add loading screen
+    colorRect(0, 0, canvas.width, canvas.height, 'red');
+    colorText("LOADING...", canvas.width/2, canvas.height/2, 'black');
+    // Load Images before everything else
+    loadImages();
 
-        
+}
 
-        
+// This is a dumbass name
+function imageLoadingDoneSoStartGame() {
+    // Set FPS and have the updateAll function refresh according to that FPS
+    var fps = 30;
+    setInterval(updateAll,1000/fps);
+    
+    setupInput();
 
-        
+
+    carReset();
+}
+
+
+// This is the function for drawing to and updating the canvas
+function updateAll() {
+    moveAll();
+    drawAll();    
+}
+
+function moveAll() {
+    carMove();
+    carTrackHandling();
+}
+
+function clearScreen() {
+    colorRect(0,0,canvas.width,canvas.height,"black");
+}
+
+function drawAll(){
+    drawTracks();
+    drawCar();
+    
+}
+
+
+
+
+
+
+
+
+
