4d3b95aea6d384d865a37d1ea12d0e1a66e0d7f1
Tej <tejgidvani@hotmail.com>
2019-07-15 03:53:34 -0500

    cont

--- js/Input.js
+++ js/Input.js
@@ -4,11 +4,7 @@ const KEY_RIGHT_ARROW = 39;
 const KEY_UP_ARROW = 38;
 const KEY_DOWN_ARROW = 40;
 
-// variable to see if the key is being held down
-var keyHeld_Gas = false;
-var keyHeld_Reverse = false;
-var keyHeld_Left = false;
-var keyHeld_Right = false;
+
 
 
 // Make mouse coordinates global
